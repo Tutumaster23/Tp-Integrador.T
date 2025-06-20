6d4516e2e9ca4edd811a7447b1f5a6204a66db49
Tutumaster23 <cristianifrancisco@gmail.com>
2025-06-20 19:21:29 -0300

    Add files via upload

--- ProyectoTPI/juego.cpp
+++ ProyectoTPI/juego.cpp
@@ -9,29 +9,29 @@ void comenzarJuego() {
     int JugadorP = tiradaInicial();
 
     if (JugadorP == 1){
-    cout << "Jugador 1: " << endl;
-    cout << endl;
-    cout << "2 dados de 12 caras" << endl;
-    int dados12[2];
-    cargarDados12(dados12, 2);
-    mostrarDados(dados12, 2);
+        cout << "Jugador 1: " << endl;
+        cout << endl;
+        cout << "2 dados de 12 caras" << endl;
+        int dados12[2];
+        cargarDados12(dados12, 2);
+        mostrarDados(dados12, 2);
 
-    cout << "6 dados de 6 caras" << endl;
-    int dados6[6];
-    cargarDados6(dados6, 6);
-    mostrarDados(dados6, 6);
+        cout << "6 dados de 6 caras" << endl;
+        int dados6[6];
+        cargarDados6(dados6, 6);
+        mostrarDados(dados6, 6);
     } else {
-    cout << "Jugador 1: " << endl;
-    cout << endl;
-    cout << "2 dados de 12 caras" << endl;
-    int dados12[2];
-    cargarDados12(dados12, 2);
-    mostrarDados(dados12, 2);
+        cout << "Jugador 1: " << endl;
+        cout << endl;
+        cout << "2 dados de 12 caras" << endl;
+        int dados12[2];
+        cargarDados12(dados12, 2);
+        mostrarDados(dados12, 2);
 
-    cout << "6 dados de 6 caras" << endl;
-    int dados6[6];
-    cargarDados6(dados6, 6);
-    mostrarDados(dados6, 6);
+        cout << "6 dados de 6 caras" << endl;
+        int dados6[6];
+        cargarDados6(dados6, 6);
+        mostrarDados(dados6, 6);
     }
 
 
